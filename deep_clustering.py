6e4d79f4f2eae3411bceb856436302513e986008
nbrady <nbrady@rdac.du.edu>
2023-12-22 12:29:10 -0700

    filtering subjects based on phenotype data

--- deep_clustering.py
+++ deep_clustering.py
@@ -9,13 +9,23 @@ from tensorflow.keras import layers, models
 from sklearn.preprocessing import normalize
 from scipy import stats
 
-# This is a test
 
 """
 data = pd.read_csv("./timeseries/sub-5062mom_realcry_denoised_bold_timeseries.csv")
 data = data.T
 data = data.drop('Unnamed: 0')
 """
+
+folder_path = '/home/nbrady/Desktop/deep_functional_net'
+phenotypes = pd.read_csv(f'{folder_path}/PHEN_MATRIX.csv')
+
+# Exclude these Subs for FD issues
+ignore_subs = [5027, 5011, 5140, 5142, 5172, 5036, 5106]
+included_subs = phenotypes['sid_rise']
+
+# Phenotype of interest
+phen_var = 'STATE_Tot_all_pn1'
+phen_data = np.array([])
 
 # Define a similarity metric (e.g., cosine similarity)
 def cosine_similarity(a, b):
@@ -44,72 +54,83 @@ def dynamic_time_warp(x, y):
 #=======================================================
 # Load in the bold timeseries to create Nx268x268 matrix
 #=======================================================
-num_subs = len(os.listdir("./timeseries"))
+num_subs = len(os.listdir(f"{folder_path}/timeseries"))
 timeseries_compression = 100
 
 #all_data = np.zeros((num_subs, 268, timeseries_compression))
 all_data = np.zeros((5, 268, timeseries_compression))
 
-for sub_id, sub in enumerate(os.listdir("./timeseries")):
+ts_data = os.listdir(f"{folder_path}/timeseries")
 
-	path = f"./timeseries/{sub}"
-	
-	# Read in the CSV
-	data = pd.read_csv(path)
-	data = data.T
-	data = data.drop('Unnamed: 0')
+for sub_id, sub in enumerate(ts_data):
 
-	#print("Shape of input data: ", data.shape)
+	# Ignore subjects with missing data or with high FD values
+	rise_id = int(sub.split('_')[0].split('-')[-1].split('m')[0])
+	sub_df = phenotypes[phenotypes['sid_rise'] == rise_id]
+	sub_phen_val = sub_df[phen_var].values[0] if len(sub_df[phen_var].values) > 0 else None
+	use_data = (rise_id not in ignore_subs) or (sub_phen_val != None)
 
-	#=================================================================
-	# Train the autoencoder for y dimension reduction for each subject
-	#=================================================================
+	if use_data == True:
 
-	np.random.seed(0)
+		phen_data = np.append(phen_data, sub_phen_val)
+		path = f"{folder_path}/timeseries/{sub}"
+		
+		# Read in the CSV
+		data = pd.read_csv(path)
+		data = data.T
+		data = data.drop('Unnamed: 0')
 
-	# Autoencoder architecture
-	input_dim = data.shape[1]
-	latent_dim = timeseries_compression  # Adjust based on your requirements
+		#print("Shape of input data: ", data.shape)
 
-	encoder = models.Sequential([
-		    layers.InputLayer(input_shape=(input_dim,)),
-		    layers.Dense(latent_dim, activation='relu'),
-		    ])
+		#=================================================================
+		# Train the autoencoder for y dimension reduction for each subject
+		#=================================================================
 
-	decoder = models.Sequential([
-		layers.InputLayer(input_shape=(latent_dim,)),
-		layers.Dense(input_dim, activation='sigmoid')
-		])
+		np.random.seed(0)
 
-	autoencoder = models.Sequential([encoder, decoder])
+		# Autoencoder architecture
+		input_dim = data.shape[1]
+		latent_dim = timeseries_compression  # Adjust based on your requirements
 
-	autoencoder.compile(optimizer='adam', loss='mse')
+		encoder = models.Sequential([
+			    layers.InputLayer(input_shape=(input_dim,)),
+			    layers.Dense(latent_dim, activation='relu'),
+			    ])
 
-	# Train the autoencoder
-	autoencoder.fit(data, data, epochs=10, batch_size=16, shuffle=True)
+		decoder = models.Sequential([
+			layers.InputLayer(input_shape=(latent_dim,)),
+			layers.Dense(input_dim, activation='sigmoid')
+			])
 
-	# Encode the data to obtain the latent representations
-	latent_rep= encoder.predict(data)
-	#print("latent_rep shape: ", latent_rep.shape)
+		autoencoder = models.Sequential([encoder, decoder])
 
-	
-	# Generate reduced dimensionality data
-	reconstructed_timeseries = decoder.predict(latent_rep)
+		autoencoder.compile(optimizer='adam', loss='mse')
 
-	# Test accuracy of the compression
-	result = autoencoder.evaluate(data, reconstructed_timeseries)
-	#print(result)
+		# Train the autoencoder
+		autoencoder.fit(data, data, epochs=10, batch_size=16, shuffle=True)
 
-	# Store the reduced data into all_data
-	all_data[sub_id] = latent_rep
+		# Encode the data to obtain the latent representations
+		latent_rep= encoder.predict(data)
+		#print("latent_rep shape: ", latent_rep.shape)
 
-	# Uncomment for testing
-	if sub_id > 3:
-		break
+		
+		# Generate reduced dimensionality data
+		reconstructed_timeseries = decoder.predict(latent_rep)
 
-print("All Data Shape: ", all_data.shape)
+		# Test accuracy of the compression
+		result = autoencoder.evaluate(data, reconstructed_timeseries)
+		#print(result)
+
+		# Store the reduced data into all_data
+		all_data[sub_id] = latent_rep
+
+		# Uncomment for testing
+		if sub_id > 3:
+			break
+
+print("All Data Shape: ", all_data.shape, "Phenotype data: ", phen_data.shape)
 flat_df = pd.DataFrame(all_data.flatten())
-flat_df.to_csv("./encoded_timeseries.csv", index=False, header=False)
+flat_df.to_csv(f"{folder_path}/encoded_timeseries.csv", index=False, header=False)
 
 #============================================ 
 # Train autoencoder for z dimension reduction 
@@ -196,4 +217,4 @@ for fc in clusters:
 print("Saving the matrix as csv")
 df = pd.DataFrame(fc_matrix)
 print(df.shape)
-df.to_csv('./testing_autoencoder_matrix.csv', index=False, header=False)
+df.to_csv(f'{folder_path}/testing_autoencoder_matrix.csv', index=False, header=False)
